79e4c01dfa07dbfa483a130e9b0a770e01a752f9
Atthiyyath <56459274+Atthiyya22194@users.noreply.github.com>
2021-04-20 16:51:45 +0700

    Add files via upload

---
+++ MyCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    public FixedTouchField touchField;
+    public float Yaxis;
+    public float Xaxis;
+    public Transform target;
+    public bool enableMobileInputs = false;
+    float RotationSensitivity = 1f;
+    float RotationMin = -30f;
+    float RotationMax = 50f;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (enableMobileInputs)
+        {
+            Xaxis += touchField.TouchDist.y * RotationSensitivity;
+            Yaxis -= touchField.TouchDist.x * RotationSensitivity;
+        }
+        else
+        {
+            Xaxis += Input.GetAxis("Mouse Y") * RotationSensitivity;
+            Yaxis -= Input.GetAxis("Mouse X") * RotationSensitivity;
+        }     
+        Xaxis = Mathf.Clamp(Xaxis, RotationMin, RotationMax);
+
+        Vector3 targetRotation = new Vector3(Xaxis, Yaxis);
+        transform.eulerAngles = targetRotation;
+
+        transform.position = target.position - transform.forward * 2f;
+    }
+}
---
+++ MyPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    float moveSpeed = 3f;
+    float smoothRotationTime = 0.25f;
+    float currentVelocity;
+    Transform cameraTransform;
+    bool isJumping = false;
+    public float jumpSpeed = 5f;
+    public bool enableMobileInputs = false;
+    public FixedJoystick joystick;
+    public FixedButton button;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = Vector2.zero;
+        if (enableMobileInputs)
+        {
+            input = new Vector2(joystick.input.x, joystick.input.y);
+        }
+        else
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+        Vector2 inputDir = input.normalized;
+
+        if(inputDir != Vector2.zero)
+        {
+            float rotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentVelocity, smoothRotationTime); ;
+        }
+        float targetSpeed = moveSpeed * inputDir.magnitude;
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Actions>().Jump();
+        }*/
+        if(inputDir.magnitude > 0)
+        {
+            GetComponent<Actions>().Run();
+        }
+        else
+        {
+            GetComponent<Actions>().Stay();
+        }
+
+        transform.Translate(transform.forward * (moveSpeed * inputDir.magnitude) * Time.deltaTime, Space.World);
+    }
+
+    public void Jump()
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+        }
+
+        GetComponent<Actions>().Jump();
+    }
+
+    private void resetIsJumping()
+    {
+        isJumping = false;
+    }
+}
